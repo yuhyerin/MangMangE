bcd4d1cc67045cdcfb23baa57172fea664172d4e
김승환 <bwin9509@naver.com>
2020-10-30 16:59:31 +0900

    Delete becauseo of error

--- backend/src/main/java/com/daeng/nyang/service/openapi/AnimalOpenApiService.java
+++
@@ -1,7 +0,0 @@
-package com.daeng.nyang.service.openapi;
-
-public interface AnimalOpenApiService {
-	
-	public void insertAnimalData();
-	public void parsingXmlData();
-}
--- frontend/src/api/url.js
+++
@@ -1,7 +0,0 @@
-export default {
-  URL: "http://localhost:8080",
-  ROUTES: {
-    submitSurvey: "/user/survey/create",
-    // updateSurvey: '/survey/update',
-  },
-};
