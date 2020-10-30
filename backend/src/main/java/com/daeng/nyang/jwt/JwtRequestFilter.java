5c1ad8dbb63840b0e64064b088c16d611d1f87b7
정현희 <wjdgusgml997@naver.com>
2020-10-30 15:31:56 +0900

    [+]PULL animalDetailFE

--- backend/src/main/java/com/daeng/nyang/jwt/JwtRequestFilter.java
+++ backend/src/main/java/com/daeng/nyang/jwt/JwtRequestFilter.java
@@ -86,7 +86,7 @@ public class JwtRequestFilter extends OncePerRequestFilter {
 				System.out.println("IllegalArgumentException in doFilterInternal");
 			}
 		}
-		System.out.println(redisTemplate.opsForValue().get(requestTokenHeader));
+
 		if (userid == null) {
 			System.out.println("userid null");
 		} else if (redisTemplate.opsForValue().get(requestTokenHeader) == null) {
