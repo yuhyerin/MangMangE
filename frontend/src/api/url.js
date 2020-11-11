97bba35376d732452ac6c44ed5f4806a48a01393
ParkKs <pks313527@naver.com>
2020-11-11 11:46:53 +0900

    merge

--- frontend/src/api/url.js
+++ frontend/src/api/url.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default {
-  URL: 'http://localhost:8080',
+  URL: 'http://localhost:8084',
   // URL: 'https://k3b306.p.ssafy.io:8080',
   ROUTES: {
     submitSurvey: '/user/survey/create',
--- frontend/src/components/Section3.vue
+++ frontend/src/components/Section3.vue
@@ -24,20 +24,22 @@
             ></object>
           </div>
           <div style="width: 50vw">
-            <div
-              style="display:flex; justify-content: center; height: 100%"
-            >
-            <div style="margin-bottom: 15px;">
-              <p style="font-size: xxx-large; color: black;"><strong>아이들을 영상으로<br>만나보세요</strong></p>
-              <!-- <p style="font-size: xxx-large; color: white;"><strong>만나보세요</strong></p> -->
-              <br>
-              <p style="font-size: large; margin-bottom: 30px; color: black;"><strong>당신을 기다리고 있어요</strong></p>
-              <br>
-              <div @click="moveToVideos">
-                <v-btn
-                  elevation="5"
-                  style="background: black; color: white;"
-                >영상 더보기</v-btn>
+            <div style="display: flex; justify-content: center; height: 100%">
+              <div style="margin-bottom: 15px">
+                <p style="font-size: xxx-large; color: black">
+                  <strong>아이들을 영상으로<br />만나보세요</strong>
+                </p>
+                <!-- <p style="font-size: xxx-large; color: white;"><strong>만나보세요</strong></p> -->
+                <br />
+                <p style="font-size: large; margin-bottom: 30px; color: black">
+                  <strong>당신을 기다리고 있어요</strong>
+                </p>
+                <br />
+                <div @click="moveToVideos">
+                  <v-btn elevation="5" style="background: black; color: white"
+                    >영상 더보기</v-btn
+                  >
+                </div>
               </div>
             </div>
           </div>
@@ -77,6 +79,7 @@ export default {
   background: rgb(1, 118, 72);
   /* background: #FFa500; */
   /* background-color: rgba(255, 165, 0, 1); */
+  /* background-color: rgb(193, 218, 204); */
   /* background: darkgray; */
   /* background: #fcf7f0; */
   /* background: #fa8290; */
