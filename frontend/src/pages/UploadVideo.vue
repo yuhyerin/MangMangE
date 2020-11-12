ee89f92df45e23ad92d79c202177a80d1326096a
kuidoli <kuidoli@hanmail.net>
2020-11-12 11:30:20 +0900

    [+]Update UploadVideo

--- frontend/src/pages/UploadVideo.vue
+++ frontend/src/pages/UploadVideo.vue
@@ -8,6 +8,7 @@
       "
     >
       <h2 style="text-align: center; padding-bottom: 12px;">동영상 업로드</h2>
+
       <div style="display: flex; height: 60px;">
         <img :src="image" alt="이미지" style="height: 60px; margin: 0 1% 1% 0; border-radius: 20px; width: 60px;">
         <v-text-field
@@ -15,85 +16,59 @@
           v-model="desertionNo"
           outlined
         ></v-text-field>
-        <!-- <v-btn 
-          small
-          elevation="0"
-          style="padding: 0; background: rgba(255, 255, 255, 0);"
-        > -->
         <v-icon style="color: green; padding: 0;"
           elevation="0"
           :disabled="desertionNoCheck!=0">
           mdi-checkbox-marked-circle
         </v-icon>
-        <!-- </v-btn> -->
       </div>
-      <p v-if="desertionNoCheck==1" style="color: orange;">일련번호는 15자입니다.</p>
-      <p v-else-if="desertionNoCheck==2" style="color: red;">존재하지 않는 일련번호입니다.</p>
+      <p v-if="desertionNoCheck==1" style="color: orange; margin-left: 70px;">일련번호는 15자입니다.</p>
+      <p v-else-if="desertionNoCheck==2" style="color: red; margin-left: 70px;">존재하지 않는 일련번호입니다.</p>
       
-      <div style="display: flex; margin-top: 25px; height: 60px;">
+      <div style="display: flex; margin-top: 35px; height: 60px;">
         <v-text-field
           placeholder="제목"
           v-model="title"
           outlined
           style="padding: 0;"
         ></v-text-field>
-        <!-- <v-btn
-          class="mx-2"
-          small
-          elevation="0"
-          style="padding: 0; background: rgba(255, 255, 255, 0);"
-        > -->
           <v-icon style="color: green; padding: 0;" 
             elevation="0"
             :disabled="title.length < 1">
             mdi-checkbox-marked-circle
           </v-icon>
-        <!-- </v-btn> -->
       </div>
 
-      <div style="display: flex; margin-top: 25px; padding-bottom: 12px;">
+      <div style="display: flex; margin-top: 35px;">
         <input type="file" ref="file" @change="selectFile" :disabled="desertionNoCheck !=0"/>
-        <!-- <v-btn
-          class="mx-2"
-          small
-          elevation="0"
-          style="padding: 0; background: rgba(255, 255, 255, 0);"
-        > -->
           <v-icon
             style="color: green;" 
             :disabled="selectedFileCheck!=3">
             mdi-checkbox-marked-circle
           </v-icon>
-        <!-- </v-btn> -->
       </div>
       <label v-if="selectedFileCheck==1" style="color:red">이미 업로드된 파일입니다</label>
       <label v-else-if="selectedFileCheck==2" style="color:red">지원하지 않는 파일 형식입니다.</label>
-      <div style="display: flex; padding-top: 12px;">
+      
+      <div style="display: flex; margin-top: 35px;">
         <v-textarea
           :counter="500"
           outlined
           name="input-7-4"
           v-model="content"
           placeholder="내용"
-          style="padding-top: 24px;"
         ></v-textarea>
-        <!-- <v-btn
-          class="mx-2"
-          small
-          elevation="0"
-          style="margin-top: 10px; padding-top: 24px; background: rgba(255, 255, 255, 0);"
-        > -->
           <v-icon style="color: green" :disabled="content.length < 1 || contentCheck === 1">
             mdi-checkbox-marked-circle
           </v-icon>
-        <!-- </v-btn> -->
       </div>
       <label v-if="contentCheck" style="color: orange;">500자 이하로 입력해주세요</label>
-      <div style="padding-top: 20px;">
+
+      <div>
         <v-btn 
           outlined
           rounded
-          :disabled="selectedFileCheck!=3 || desertionNoCheck!=0 || title.length < 1 || content.length < 1 || contentCheck === 1"
+          :disabled="selectedFileCheck != 3 || desertionNoCheck != 0 || title.length < 1 || content.length < 1 || contentCheck === 1"
           @click="upload"
         >등록하기</v-btn>
       </div>
@@ -113,15 +88,15 @@ export default {
 
   data(){
     return{
-      desertionNo:'',
-      title:'',
+      desertionNo: '',
+      title: '',
       file: undefined,
-      content:'',
+      content: '',
       selectedFiles: false,
-      desertionNoCheck: 1,
+      desertionNoCheck: 3,
       selectedFileCheck: 0,
       image: require(`@/assets/image/merong1.png`),
-      error:{
+      error: {
         message:'일련번호를 확인해주세요',
       },
       contentCheck: 0,
@@ -143,19 +118,19 @@ export default {
             "Authorization" : this.$cookies.get("accessToken")
           }
         })
-        .then((res)=>{
+        .then((res) => {
           console.log(res)
-          if(res.status==202){
+          if(res.status==202) {
             console.log(res)
-            this.desertionNoCheck=2;  // 존재하지 않는 일련번호입니다.
+            this.desertionNoCheck = 2;  // 존재하지 않는 일련번호입니다.
           }
-          else{
+          else {
             this.image = res.data.image;
             this.desertionNoCheck = 0;
             this.error.message = ''
           }
         })
-        .catch((err)=>{
+        .catch((err) => {
           console.log(err)
         });
         });
@@ -163,26 +138,26 @@ export default {
     },
 
     file() {
-      if(this.file[0].name.slice(-3)=='mp4'){
+      if(this.file[0].name.slice(-3) == 'mp4'){
         SERVER.tokenCheck(() => {
         axios.get(SERVER.URL+'/admin/upload/checkFile',{
           params:{
-            fileName : this.desertionNo+"_"+this.file[0].name
+            fileName : this.desertionNo + "_" + this.file[0].name
           },
           headers:{
-            "Authorization" : this.$cookies.get("accessToken")
+            "Authorization": this.$cookies.get("accessToken")
           }
         })
         .then((res)=>{
           console.log(res)
-          if(res.status==202){
+          if(res.status == 202) {
             this.selectedFileCheck = 1;
           }
-          else{
+          else {
             this.selectedFileCheck = 3;
           }
         })
-        .catch((err)=>{
+        .catch((err) => {
           console.log(err)
         })
         });
@@ -215,14 +190,14 @@ export default {
         SERVER.tokenCheck(()=>{
           axios.post(SERVER.URL + '/admin/uploadVideo',
             {
-              "desertion_no" : this.desertionNo,
-              "title" : this.title,
-              "content" : this.content,
-              "filepath" : this.desertionNo+'_'+this.file[0].name
+              "desertion_no": this.desertionNo,
+              "title": this.title,
+              "content": this.content,
+              "filepath": this.desertionNo+'_'+this.file[0].name
             },
             {
               headers:{
-                "Authorization" : this.$cookies.get("accessToken"),
+                "Authorization": this.$cookies.get("accessToken"),
                 "contentType": 'application/json'
               }
             }
@@ -230,26 +205,26 @@ export default {
           .then((res)=>{
             console.log(res.data)
             formData.append("mfile", this.file[0], res.data.uid+"_"+this.file[0].name);
-            if(res.data.success==true){
+            if(res.data.success == true) {
               axios.post(SERVER.URL+'/admin/upload',formData,
             {
               headers: {
-                  "Authorization" : this.$cookies.get("accessToken"),
-                  "content-Type" : "multipart/form-data; charset=utf-8; boundary='calculated when request is sent';"
+                  "Authorization": this.$cookies.get("accessToken"),
+                  "content-Type": "multipart/form-data; charset=utf-8; boundary='calculated when request is sent';"
               }
             })
-            .then((res)=>{
+            .then((res) => {
               console.log('RES : ',res)
               alert('등록되었습니다')
               this.$router.push("/videos")
             })
-            .catch((err)=>{
-              console.log('ERROR : ',err)
+            .catch((err) => {
+              console.log('ERROR: ',err)
             })
             }
           })
-          .catch((err)=>{
-            console.log('ERRORERROR : ',err)
+          .catch((err) => {
+            console.log('ERRORERROR: ',err)
           })
         });
     }
