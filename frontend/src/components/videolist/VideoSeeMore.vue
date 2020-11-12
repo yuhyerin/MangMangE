4b605614567a0380ca0f2c519b3da1ed27e1cb03
soy0vng <soy2726@naver.com>
2020-11-12 17:44:52 +0900

    CSS

--- frontend/src/components/videolist/VideoSeeMore.vue
+++ frontend/src/components/videolist/VideoSeeMore.vue
@@ -3,23 +3,23 @@
     <v-row style="margin: 10px 0 5px 20px">
       <h2>총 동영상 {{ videos_cnt }}개</h2>
     </v-row>
-    <v-row v-for="video in videos" :key="video.id" style="padding: 0 20px 0 20px">
-        <v-col cols="6">
-          <video
-          :src="require(`@/assets/videos/${video.filepath}`)"
-          type="video/mp4"
-          controls
-          style="max-height: 288px; width:100%; height:100%;"
-        ></video>
-        </v-col>
-        <v-col cols="5" style="margin-left: 15px">
-          <h2 class="video-info" @click="moveToVideoDetail(video.uid)" style="margin-bottom: 10px;">{{ video.title }}</h2>
-          <p style="color: gray; font-size: 0.9rem; margin-bottom: 10px">
-            <i class="far fa-user fa-xs" style="margin-right: 5px;"></i>{{ video.writer }} |
-            <i class="fas fa-calendar-day fa-xs" style="margin-left: 5px"></i> {{ video.regtime }}
-          </p>
-          <p class="video-info" @click="moveToVideoDetail(video.uid)" style="line-height: 150%;">{{ video.content }}</p>
-        </v-col>
+    <v-row v-for="video in videos" :key="video.id" style="padding: 0 20px 0 20px;">
+      <v-col cols="6">
+        <video
+        :src="require(`@/assets/videos/${video.filepath}`)"
+        type="video/mp4"
+        controls
+        style="max-height: 288px; width:100%; height:100%;"
+      ></video>
+      </v-col>
+      <v-col cols="5" style="margin-left: 15px;">
+        <h2 class="video-info" @click="moveToVideoDetail(video.uid)" style="margin-bottom: 10px;">{{ video.title }}</h2>
+        <p style="color: gray; font-size: 0.9rem; margin-bottom: 10px">
+          <i class="far fa-user fa-xs" style="margin-right: 5px;"></i>{{ video.writer }} |
+          <i class="fas fa-calendar-day fa-xs" style="margin-left: 5px"></i> {{ video.regtime }}
+        </p>
+        <p class="video-info" @click="moveToVideoDetail(video.uid)" style="line-height: 150%;">{{ video.content }}</p>
+      </v-col>
     </v-row>
     <ScrollToTop />
   </div>
